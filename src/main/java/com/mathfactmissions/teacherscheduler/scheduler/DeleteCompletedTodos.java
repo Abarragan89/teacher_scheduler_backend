d9d328ee59c69c8398846e564935d432c69d8412
Anthony Barragan <anthony.bar.89@gmail.com>
2026-03-20 09:33:51 -0700

    added annotations with deleting old todods

--- src/main/java/com/mathfactmissions/teacherscheduler/scheduler/DeleteCompletedTodos.java
+++ src/main/java/com/mathfactmissions/teacherscheduler/scheduler/DeleteCompletedTodos.java
@@ -2,8 +2,10 @@ package com.mathfactmissions.teacherscheduler.scheduler;
 
 import com.mathfactmissions.teacherscheduler.repository.TodoRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
@@ -14,6 +16,8 @@ public class DeleteCompletedTodos {
     
     private final TodoRepository todoRepository;
     
+    @Modifying
+    @Transactional
     @Scheduled(cron = "0 0 * * * *") // every hour
     public void purgeCompletedTodos() {
         Instant cutoff = Instant.now().minus(24, ChronoUnit.HOURS);
